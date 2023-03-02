4c6f46975ad274a5d22c5e123ed492601c7e2fcf
krialex <56829037+krialex@users.noreply.github.com>
2023-03-02 11:03:32 +0100

    added some style

--- js/question-2.js
+++ js/question-2.js
@@ -1,4 +1,3 @@
-//const apiKey = 9413a4dcf7c54728b1a4c54436efeed0;
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=9413a4dcf7c54728b1a4c54436efeed0";
 
 const resultContainer = document.querySelector(".results");
@@ -21,12 +20,11 @@ async function getGames() {
                 break;
             }
 
-            resultContainer.innerHTML += `<div class="result">Name: ${gameInfo[i].name}. 
-            Rating: ${gameInfo[i].rating}. Number of tags: ${gameInfo[i].tags.length} </div>`;
+            resultContainer.innerHTML += `<div class="result"><p>Name: ${gameInfo[i].name}</p> 
+            <p>Rating: ${gameInfo[i].rating}</p><p>Number of tags: ${gameInfo[i].tags.length}</p></div>`;
         }
             
     } catch (error) {
-       console.log("ERROR");
        resultContainer.innerHTML = `<div class="error">There was an error</div>`;
     }
 }
